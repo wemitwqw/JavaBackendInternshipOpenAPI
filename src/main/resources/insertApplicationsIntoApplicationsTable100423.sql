9a1dba93f65802df7edea62f4213f77c1c2111fb
wemit <55079623+wemitwqw@users.noreply.github.com>
2023-04-12 15:05:03 +0300

    upd

--- src/main/resources/insertApplicationsIntoApplicationsTable100423.sql
+++ src/main/resources/insertApplicationsIntoApplicationsTable100423.sql
@@ -5,11 +5,3 @@ INSERT INTO application (application_state, updated_on, candidate_id, interview_
 INSERT INTO application (application_state, updated_on, candidate_id) VALUES ('NEW', '2023-04-10T14:20:00.01+03:00', 5);
 INSERT INTO application (application_state, updated_on, candidate_id, interview_id) VALUES ('INTERVIEW', '2023-04-11T14:50:00.01+03:00', 6, 2);
 INSERT INTO application (application_state, updated_on, candidate_id) VALUES ('NEW', '2023-04-11T19:29:00.01+03:00', 7);
-
--- INSERT INTO application (application_state, updated_on, candidate_id) VALUES ('REJECTED', '2023-04-10T10:20:00.01+03:00', 1);
--- INSERT INTO application (application_state, updated_on, candidate_id) VALUES ('INTERVIEW', '2023-04-10T11:20:00.01+03:00', 2);
--- INSERT INTO application (application_state, updated_on, candidate_id) VALUES ('HIRED', '2023-04-10T12:20:00.01+03:00', 3);
--- INSERT INTO application (application_state, updated_on, candidate_id) VALUES ('INTERVIEW', '2023-04-10T13:20:00.01+03:00', 4);
--- INSERT INTO application (application_state, updated_on, candidate_id) VALUES ('NEW', '2023-04-10T14:20:00.01+03:00', 5);
--- INSERT INTO application (application_state, updated_on, candidate_id) VALUES ('INTERVIEW', '2023-04-11T14:50:00.01+03:00', 6);
--- INSERT INTO application (application_state, updated_on, candidate_id) VALUES ('NEW', '2023-04-11T19:29:00.01+03:00', 7);
--- src/main/resources/insertInterviewsIntoInterviewsTable110423.sql
+++ src/main/resources/insertInterviewsIntoInterviewsTable110423.sql
@@ -1,6 +1,3 @@
 INSERT INTO interview (interview_date_time) VALUES ('2023-04-18T15:00:00.00+03:00');
 INSERT INTO interview (interview_date_time) VALUES ('2023-04-18T15:40:00.00+03:00');
 
--- INSERT INTO interview (interview_date_time, application_id) VALUES ('2023-04-18T15:00:00.00+03:00', 4);
--- INSERT INTO interview (interview_date_time, application_id) VALUES ('2023-04-18T15:40:00.00+03:00', 6);
-
